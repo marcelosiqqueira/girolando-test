8f5378b2a6939a9b74f5edea5be67baadd28e257
Marcelo Siqueira <marcelosiqqueira@gmail.com>
2025-02-05 11:13:09 -0300

    feat: create weekly card component with custom styles

---
+++ resources/views/weather/components/weekly-card.blade.php
@@ -0,0 +1,35 @@
+@vite('resources/css/weekly-card.css')
+
+<div class="weekly-card-container card" style="justify-content: center;">
+    <h1>Previsão Semanal:</h1>
+
+    <div class="forecast-day-container">
+        <img src="{{ asset('assets/icons/sun.svg') }}" alt="Forecast Icon">
+        <span>30°C</span>
+        <span>Sexta - Feira, 04 Fev</span>
+    </div>
+
+    <div class="forecast-day-container">
+        <img src="{{ asset('assets/icons/sun.svg') }}" alt="Forecast Icon">
+        <span>30°C</span>
+        <span>Sexta - Feira, 04 Fev</span>
+    </div>
+
+    <div class="forecast-day-container">
+        <img src="{{ asset('assets/icons/sun.svg') }}" alt="Forecast Icon">
+        <span>30°C</span>
+        <span>Sexta - Feira, 04 Fev</span>
+    </div>
+
+    <div class="forecast-day-container">
+        <img src="{{ asset('assets/icons/sun.svg') }}" alt="Forecast Icon">
+        <span>30°C</span>
+        <span>Sexta - Feira, 04 Fev</span>
+    </div>
+
+    <div class="forecast-day-container">
+        <img src="{{ asset('assets/icons/sun.svg') }}" alt="Forecast Icon">
+        <span>30°C</span>
+        <span>Sexta - Feira, 04 Fev</span>
+    </div>
+</div>
