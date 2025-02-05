357bf314868efe341ebafe7029a0efd580741908
Marcelo Siqueira <marcelosiqqueira@gmail.com>
2025-02-05 11:13:29 -0300

    feat: create hourly forecast component with custom styles

---
+++ resources/views/weather/components/hourly-forecast.blade.php
@@ -0,0 +1,54 @@
+@vite('resources/css/hourly-forecast.css')
+
+<div class="hourly-forecast-container card">
+    <h1>Previsão Horária:</h1>
+
+    <div class="forecast-info-container">
+
+        <div class="hour-info-container sun-background">
+            <span>20:00</span>
+            <img src="{{ asset('assets/icons/sun.svg') }}" alt="Forecast Icon">
+            <span>26°C</span>
+            <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Forecast Icon">
+            <span>3km/hC</span>
+        </div>
+
+        <div class="hour-info-container sun-background">
+            <span>20:00</span>
+            <img src="{{ asset('assets/icons/sun.svg') }}" alt="Forecast Icon">
+            <span>26°C</span>
+            <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Forecast Icon">
+            <span>3km/hC</span>
+        </div>
+
+        <div class="hour-info-container sun-background">
+            <span>20:00</span>
+            <img src="{{ asset('assets/icons/sun.svg') }}" alt="Forecast Icon">
+            <span>26°C</span>
+            <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Forecast Icon">
+            <span>3km/hC</span>
+        </div>
+
+        <div class="hour-info-container rain-background">
+            <span>20:00</span>
+            <img src="{{ asset('assets/icons/sun.svg') }}" alt="Forecast Icon">
+            <span>26°C</span>
+            <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Forecast Icon">
+            <span>3km/hC</span>
+        </div>
+
+        <div class="hour-info-container rain-background">
+            <span>20:00</span>
+            <img src="{{ asset('assets/icons/sun.svg') }}" alt="Forecast Icon">
+            <span>26°C</span>
+            <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Forecast Icon">
+            <span>3km/hC</span>
+        </div>
+
+
+    </div>
+
+
+
+
+</div>
