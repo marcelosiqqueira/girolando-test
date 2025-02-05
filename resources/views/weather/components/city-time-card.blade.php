d2bb487d724fdc93fb0de7978ed66a307604f2eb
Marcelo Siqueira <marcelosiqqueira@gmail.com>
2025-02-05 11:12:53 -0300

    feat: create city time card component with custom styles

---
+++ resources/views/weather/components/city-time-card.blade.php
@@ -0,0 +1,10 @@
+@vite('resources/css/city-time-card.css')
+
+<div class="city-time-card-container card">
+    <h2>Uberaba</h2>
+
+    <div class="time-calendar-container">
+        <h1>09:03</h1>
+        <span>Terça - Feira, 02 Fev</span>
+    </div>
+</div>
