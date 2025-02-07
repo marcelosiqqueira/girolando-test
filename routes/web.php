9e00d05749a057e4b42dad7f505f996cdfd342d1
Marcelo Siqueira <marcelosiqqueira@gmail.com>
2025-02-07 12:27:41 -0300

    feat: created weather routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('weather/index');
-});
+Route::get('/', [WeatherController::class, 'index']);
+
+Route::get('/search-city', [WeatherController::class, 'searchCity'])->name('search.city');
