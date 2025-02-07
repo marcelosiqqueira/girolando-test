5bc100ff93be5abd0ce4e7d88bb419afbbf21b2f
Marcelo Siqueira <marcelosiqqueira@gmail.com>
2025-02-07 12:25:51 -0300

    feat: implements weatherData controller to blade view

--- resources/views/weather/index.blade.php
+++ resources/views/weather/index.blade.php
@@ -1,15 +1,20 @@
-
 @vite('resources/css/app.css')
 @vite('resources/css/weather.css')
 
 <div class="index-container">
-
     @include('components.app-header')
 
     <div class="weather-dashboard">
-        @include('weather.components.city-time-card')
-        @include('weather.components.info-card')
-        @include('weather.components.weekly-card')
-        @include('weather.components.hourly-forecast')
+        @if ($weatherData)
+            @include('weather.components.city-time-card', ['weather' => $weatherData])
+            @include('weather.components.info-card', ['weather' => $weatherData])
+            @include('weather.components.weekly-card', ['weather' => $weatherData])
+            @include('weather.components.hourly-forecast', ['weather' => $weatherData])
+        @else
+            <div class="error-message">
+                <h2>❌ Erro ao obter os dados do tempo.</h2>
+                <p>Por favor, tente novamente mais tarde.</p>
+            </div>
+        @endif
     </div>
 </div>
